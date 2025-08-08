e01666cb36102d1ee3ca401c7d69f52e83f384f8
MahaManisha <mahamanisha2005@gmail.com>
2025-08-08 15:15:44 +0530

    Added server.js,models and routes for CRUD api

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://MahaManisha:Maha5900@cluster0.4ge2sqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+const itemRoutes= require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
